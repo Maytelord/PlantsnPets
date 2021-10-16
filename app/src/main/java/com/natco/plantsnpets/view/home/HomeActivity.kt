3d66ce71df5f85cc9fd85ee428f9deb92108e126
Maytelord <m_ay_225@hotmail.com>
2021-10-15 19:14:11 -0500

    create base app

--- app/src/main/java/com/natco/plantsnpets/view/home/HomeActivity.kt
+++ app/src/main/java/com/natco/plantsnpets/view/home/HomeActivity.kt
@@ -1,4 +1,15 @@
 package com.natco.plantsnpets.view.home
 
-class HomeActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.natco.plantsnpets.R
+import com.natco.plantsnpets.databinding.ActivityHomeBinding
+
+class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_home)
+    }
 }
--- app/src/main/java/com/natco/plantsnpets/view/home/fragment/SearchFragment.kt
+++ app/src/main/java/com/natco/plantsnpets/view/home/fragment/SearchFragment.kt
@@ -1,4 +1,4 @@
-package com.natco.plantsnpets.view.auth
+package com.natco.plantsnpets.view.home.fragment
 
 import android.content.Intent
 import android.os.Bundle
@@ -7,22 +7,20 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.natco.plantsnpets.databinding.LoginFragmentBinding
+import com.natco.plantsnpets.databinding.SearchFragmentBinding
 import com.natco.plantsnpets.view.home.HomeActivity
 
-class LoginFragment : Fragment()  {
+class SearchFragment : Fragment(){
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val binding = LoginFragmentBinding.inflate(inflater, container, false)
+        val binding = SearchFragmentBinding.inflate(inflater, container, false)
         context ?: return binding.root
 
 
-        binding.loginBTN.setOnClickListener {
-            val intent = Intent(context, HomeActivity::class.java)
-            requireActivity().startActivity(intent)
-        }
+
 
         return binding.root
     }
